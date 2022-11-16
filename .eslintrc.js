8d6814ae45c9d7d8515c4781cb7775fa76365804
1327472 <jaeyulim@lotte.net>
2022-11-16 12:06:19 +0900

    ESlint rules 설정추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     // vue
     // 'plugin:vue/vue3-essential', //Lv1
-    'plugin:vue/vue3-stronly-recommended', //Lv2
+    'plugin:vue/vue3-strongly-recommended', //Lv2
     // 'plugin:vue/vue3-recommended', //Lv 3
     // js
     'eslint:recommended'
@@ -14,7 +14,20 @@ module.exports = {
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
-  ruels: {
-    
+  rules: {
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "always",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }],
+    "vue/multi-word-component-names": 0
   }
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,5 @@
 <template>
-  <img src="~assets/logo.png" alt="HEROPY">
-</template>
+  <img
+    src="~assets/logo.png"
+    alt="HEROPY" />
+</template> 
